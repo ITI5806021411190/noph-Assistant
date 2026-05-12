5f4b038b8584f4489738b44f1a0591fb7db74d65
วรวงศ์ คูณหอม <wongnazaipot@gmail.com>
2026-05-13 02:36:59 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haos-v68-2-shell';
+const CACHE_NAME = 'haos-v68-3-shell';
 const CORE = ['/', '/index.html', '/public.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => {}));
