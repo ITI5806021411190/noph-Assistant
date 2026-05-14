59dcc1dab98a601dfad4ebc02bbb9a08949400c9
วรวงศ์ คูณหอม <wongnazaipot@gmail.com>
2026-05-14 16:57:26 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haos-v68-9-shell-fix6-newbase';
+const CACHE_NAME = 'haos-v68-9-shell-fix7-tab-whitelist';
 const CORE = ['/', '/index.html', '/public.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => {}));
