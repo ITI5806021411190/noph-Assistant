4134bfcc6457da2bb0596709651bf8ec6af72eb8
วรวงศ์ คูณหอม <wongnazaipot@gmail.com>
2026-05-14 17:44:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'haos-v68-9-shell-fix7-tab-whitelist';
-const CORE = ['/', '/index.html', '/public.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const CACHE_NAME = 'haos-v69-ui-enhancements';
+const CORE = ['/', '/index.html', '/public.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo-moph.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => {}));
   self.skipWaiting();
