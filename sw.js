d6080160f529739f194e0d2d6a0ba28308c2d692
วรวงศ์ คูณหอม <wongnazaipot@gmail.com>
2026-05-15 11:05:22 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haos-v69-1-white-screen-fix';
+const CACHE_NAME = 'haos-v69-2-report-schedule-polish';
 const CORE = ['/', '/index.html', '/public.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo-moph.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => {}));
