facc436162d7bad21746fbaf6929d1fb7c0f2899
วรวงศ์ คูณหอม <wongnazaipot@gmail.com>
2026-05-15 15:17:58 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haos-v70-1-tab-survey-filter-hotfix';
+const CACHE_NAME = 'haos-v70-2-schedule-report-it-ux';
 const CORE = ['/', '/index.html', '/public.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo-moph.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => {}));
