7eed2b0f75d8025e3c3d27bfcd8881d464352d51
วรวงศ์ คูณหอม <wongnazaipot@gmail.com>
2026-05-15 10:20:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haos-v69-ui-enhancements';
+const CACHE_NAME = 'haos-v69-1-fixes';
 const CORE = ['/', '/index.html', '/public.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo-moph.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => {}));
