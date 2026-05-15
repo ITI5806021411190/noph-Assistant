b6d482b93695205d35182fb2951ab4a63d97c8fa
วรวงศ์ คูณหอม <wongnazaipot@gmail.com>
2026-05-15 16:09:49 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haos-v70-2-schedule-report-it-ux';
+const CACHE_NAME = 'haos-v70-3-stability-public-it-forms';
 const CORE = ['/', '/index.html', '/public.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo-moph.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => {}));
