944b2d865b8e9fa73fc8cb2f55cb1059c6ccbc03
วรวงศ์ คูณหอม <wongnazaipot@gmail.com>
2026-05-21 16:14:31 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haos-v70-24-itrepair-requester-head-fallback';
+const CACHE_NAME = 'haos-v70-25-itrepair-upload-form-preserve';
 const CORE = ['/', '/index.html', '/public.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo-moph.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => {}));
