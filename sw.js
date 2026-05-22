05ae2c8d54ad839f885e379946120a3ce0718b3f
วรวงศ์ คูณหอม <wongnazaipot@gmail.com>
2026-05-22 15:20:39 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haos-v70-26-itrepair-reviewer-role';
+const CACHE_NAME = 'haos-v70-27-pin-reset-user-pin-approval';
 const CORE = ['/', '/index.html', '/public.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo-moph.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => {}));
