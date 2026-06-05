e943cae4ea2679994834a11ab1b067070c913a82
วรวงศ์ คูณหอม <wongnazaipot@gmail.com>
2026-06-05 11:26:24 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haos-v70-34-notification-action-fix';
+const CACHE_NAME = 'haos-v70-35-workspace-permissions-required-fields';
 const CORE = ['/', '/index.html', '/public.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo-moph.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => {}));
