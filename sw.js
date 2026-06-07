28bc7fc5c2415512aead71eb79bfed6eeb84d972
วรวงศ์ คูณหอม <wongnazaipot@gmail.com>
2026-06-08 00:40:53 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haos-v70-41-workspace-response-editor-ux';
+const CACHE_NAME = 'haos-v70-42-schedule-workspace-polish';
 const CORE = ['/', '/index.html', '/public.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo-moph.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => {}));
