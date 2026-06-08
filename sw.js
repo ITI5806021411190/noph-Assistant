2fe96448c695a4034df5d4ddbdbbb33bb9ae1fa8
วรวงศ์ คูณหอม <wongnazaipot@gmail.com>
2026-06-08 18:19:57 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haos-v70-48-booking-security-emeeting';
+const CACHE_NAME = 'haos-v70-49-final-polish';
 const CORE = ['/', '/index.html', '/public.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo-moph.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => {}));
