f2fab3b0b40fe45b9add031646edc62f34fd6680
วรวงศ์ คูณหอม <wongnazaipot@gmail.com>
2026-06-09 20:22:21 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haos-v70-53-remote-support-mvp';
+const CACHE_NAME = 'haos-v70-54-remote-support-role-ux';
 const CORE = ['/', '/index.html', '/public.html', '/remote.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo-moph.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => {}));
